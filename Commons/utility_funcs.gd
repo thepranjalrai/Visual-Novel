d690bed7e22cb77d2a336cd65e1d4259cabd97bc
Pranjal Rai <thepranjalrai@gmail.com>
2021-02-09 21:19:31 +0530

    Initial Commit

---
+++ Commons/utility_funcs.gd
@@ -0,0 +1,24 @@
+extends Node
+
+static func traverse_dir(path: String, recursive := false):
+	var paths = []
+	var dir = Directory.new()
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+				paths.append(path + file_name)
+				if(recursive):
+					if (file_name != "." and file_name != ".."):
+						traverse_dir(path + file_name + "/")
+			else:
+				print("Found file: " + file_name)
+				paths.append(path + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+		
+	return paths
+	pass
---
+++ Other Scenes/f_s_intro/s_intro.gd
@@ -0,0 +1,21 @@
+extends Control
+
+func _ready():
+	$skip_intro_button.connect("pressed", self, "_skip_intro")
+	$clear_progress_button.connect("pressed", self, "_clear_progress")
+	pass # Replace with function body.
+
+func _clear_progress():
+	var paths = UtilityFuncs.traverse_dir("user://")
+	print(paths)
+	
+	var deleter = Directory.new()
+	for path in paths:
+		if (path[-1] != "."):
+			print("Deleting ", path)
+			deleter.remove(path)
+	pass
+
+func _skip_intro():
+	print("Changing scene..")
+	pass
